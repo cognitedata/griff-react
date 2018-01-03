9e3c3586f82dbcc864285f5f9bfca6a5256c62e7
Martin Røed <martin.roed@cognite.com>
2018-01-03 09:36:13 +0100

    Removed builds.

--- es/index.js
+++
@@ -1,6 +0,0 @@
-export { default as DataProvider } from './components/DataProvider';
-export { default as ChartContainer } from './components/ChartContainer';
-export { default as ContextChart } from './components/ContextChart';
-export { default as LineChart } from './components/LineChart';
-export { default as Line } from './components/Line';
-export { default as Axis } from './components/Axis';
