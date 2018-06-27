86e41bd8e592099be1a0298092f870a1c2a6f713
Martin Røed <martin.roed@cognite.com>
2018-06-27 09:20:42 +0200

    Fixed annotation story.

--- stories/index.js
+++ stories/index.js
@@ -404,6 +404,7 @@ storiesOf('LineChart', module)
       const series = staticLoader({
         id: 1,
         reason: 'MOUNTED',
+        baseDomain: staticBaseDomain,
       }).data;
       const exampleAnnotations = [
         {
