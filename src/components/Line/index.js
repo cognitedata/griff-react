26e7064aa81a6ba53371ccfa7ccb67e940572284
Evan Charlton <evancharlton@gmail.com>
2018-12-11 10:35:26 +0100

    Update src/components/Line/index.js
    Co-Authored-By: valerii-cognite <38251534+valerii-cognite@users.noreply.github.com>

--- src/components/Line/index.js
+++ src/components/Line/index.js
@@ -31,7 +31,7 @@ const Line = ({
       .curve(d3.curveStepAfter)
       .x(d => boundedSeries(xScale(xAxisAccessor(d))))
       .y(d => boundedSeries(yScale(yAccessor(d))));
-    if (drawPoints !== false && y0Accessor && y1Accessor) {
+    if (drawPoints !== true && y0Accessor && y1Accessor) {
       area = d3
         .area()
         .curve(d3.curveStepAfter)
