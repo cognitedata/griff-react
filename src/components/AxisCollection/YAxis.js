692675c8163563db990505e81dd5009a0466010f
Evan Charlton <evancharlton@gmail.com>
2019-01-21 12:35:01 +0100

    Convert YAxis to a stateless component (#245)
    The YAxis component does not contain state -- convert it to a stateless
    component.

--- src/components/AxisCollection/YAxis.js
+++ src/components/AxisCollection/YAxis.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import * as d3 from 'd3';
 import PropTypes from 'prop-types';
 import { createYScale } from '../../utils/scale-helpers';
@@ -24,7 +24,6 @@ const propTypes = {
   ticks: PropTypes.number,
 
   // These are populated by Griff.
-  updateDomains: GriffPropTypes.updateDomains.isRequired,
   subDomainsByItemId: GriffPropTypes.subDomainsByItemId.isRequired,
 };
 
@@ -39,201 +38,224 @@ const defaultProps = {
   ticks: 0,
 };
 
-class YAxis extends Component {
-  getItem = () =>
-    this.props.series ? this.props.series : this.props.collection;
+const getItem = (series, collection) => series || collection;
 
-  getLineProps = ({ tickSizeInner, strokeWidth }) => {
-    const { width, yAxisPlacement } = this.props;
-    switch (yAxisPlacement) {
-      case AxisPlacement.LEFT:
-        return {
-          x1: width - strokeWidth,
-          x2: width - strokeWidth - tickSizeInner,
-          y1: strokeWidth / 2,
-          y2: strokeWidth / 2,
-        };
-      case AxisPlacement.BOTH:
-        throw new Error(
-          'BOTH is not a valid option for YAxis -- please specify RIGHT or LEFT'
-        );
-      case AxisPlacement.RIGHT:
-      case AxisPlacement.UNSPECIFIED:
-      default:
-        return {
-          x1: 0,
-          x2: tickSizeInner,
-          y1: strokeWidth / 2,
-          y2: strokeWidth / 2,
-        };
-    }
-  };
+const getLineProps = ({
+  strokeWidth,
+  tickSizeInner,
+  width,
+  yAxisPlacement,
+}) => {
+  switch (yAxisPlacement) {
+    case AxisPlacement.LEFT:
+      return {
+        x1: width - strokeWidth,
+        x2: width - strokeWidth - tickSizeInner,
+        y1: strokeWidth / 2,
+        y2: strokeWidth / 2,
+      };
+    case AxisPlacement.BOTH:
+      throw new Error(
+        'BOTH is not a valid option for YAxis -- please specify RIGHT or LEFT'
+      );
+    case AxisPlacement.RIGHT:
+    case AxisPlacement.UNSPECIFIED:
+    default:
+      return {
+        x1: 0,
+        x2: tickSizeInner,
+        y1: strokeWidth / 2,
+        y2: strokeWidth / 2,
+      };
+  }
+};
 
-  getPathString = ({ tickSizeOuter, range, strokeWidth }) => {
-    const { yAxisPlacement, width } = this.props;
-    switch (yAxisPlacement) {
-      case AxisPlacement.LEFT:
-        return [
-          `M${width - tickSizeOuter},${range[0] - strokeWidth}`,
-          `H${width - strokeWidth}`,
-          `V${range[1]}`,
-          `H${width - tickSizeOuter}`,
-        ].join('');
-      case AxisPlacement.BOTH:
-        throw new Error(
-          'BOTH is not a valid option for YAxis -- please specify RIGHT or LEFT'
-        );
-      case AxisPlacement.RIGHT:
-      case AxisPlacement.UNSPECIFIED:
-      default:
-        return [
-          // Move to this (x,y); start drawing
-          `M${tickSizeOuter},${range[0] - strokeWidth}`,
-          // Draw a horizontal line half strokeWidth long
-          `H${strokeWidth / 2}`,
-          // Draw a vertical line from bottom to top
-          `V${range[1]}`,
-          // Finish with another horizontal line
-          `H${tickSizeOuter}`,
-        ].join('');
-    }
-  };
+const getTextProps = ({
+  strokeWidth,
+  tickPadding,
+  tickSizeInner,
+  width,
+  yAxisPlacement,
+}) => {
+  switch (yAxisPlacement) {
+    case AxisPlacement.LEFT:
+      return {
+        x: Math.max(width - tickSizeInner, 0) - tickPadding,
+        y: strokeWidth / 2,
+      };
+    case AxisPlacement.BOTH:
+      throw new Error(
+        'BOTH is not a valid option for YAxis -- please specify RIGHT or LEFT'
+      );
+    case AxisPlacement.RIGHT:
+    case AxisPlacement.UNSPECIFIED:
+    default:
+      return {
+        x: Math.max(tickSizeInner, 0) + tickPadding,
+        y: strokeWidth / 2,
+      };
+  }
+};
 
-  getTextAnchor = () => {
-    const { yAxisPlacement } = this.props;
-    switch (yAxisPlacement) {
-      case AxisPlacement.LEFT:
-        return 'end';
-      case AxisPlacement.BOTH:
-        throw new Error(
-          'BOTH is not a valid option for YAxis -- please specify RIGHT or LEFT'
-        );
-      case AxisPlacement.RIGHT:
-      case AxisPlacement.UNSPECIFIED:
-      default:
-        return 'start';
-    }
-  };
+const getPathString = ({
+  range,
+  strokeWidth,
+  tickSizeOuter,
+  width,
+  yAxisPlacement,
+}) => {
+  switch (yAxisPlacement) {
+    case AxisPlacement.LEFT:
+      return [
+        `M${width - tickSizeOuter},${range[0] - strokeWidth}`,
+        `H${width - strokeWidth}`,
+        `V${range[1]}`,
+        `H${width - tickSizeOuter}`,
+      ].join('');
+    case AxisPlacement.BOTH:
+      throw new Error(
+        'BOTH is not a valid option for YAxis -- please specify RIGHT or LEFT'
+      );
+    case AxisPlacement.RIGHT:
+    case AxisPlacement.UNSPECIFIED:
+    default:
+      return [
+        // Move to this (x,y); start drawing
+        `M${tickSizeOuter},${range[0] - strokeWidth}`,
+        // Draw a horizontal line half strokeWidth long
+        `H${strokeWidth / 2}`,
+        // Draw a vertical line from bottom to top
+        `V${range[1]}`,
+        // Finish with another horizontal line
+        `H${tickSizeOuter}`,
+      ].join('');
+  }
+};
 
-  getTextProps = ({ tickSizeInner, tickPadding, strokeWidth }) => {
-    const { width, yAxisPlacement } = this.props;
-    switch (yAxisPlacement) {
-      case AxisPlacement.LEFT:
-        return {
-          x: Math.max(width - tickSizeInner, 0) - tickPadding,
-          y: strokeWidth / 2,
-        };
-      case AxisPlacement.BOTH:
-        throw new Error(
-          'BOTH is not a valid option for YAxis -- please specify RIGHT or LEFT'
-        );
-      case AxisPlacement.RIGHT:
-      case AxisPlacement.UNSPECIFIED:
-      default:
-        return {
-          x: Math.max(tickSizeInner, 0) + tickPadding,
-          y: strokeWidth / 2,
-        };
-    }
-  };
+const getTextAnchor = ({ yAxisPlacement }) => {
+  switch (yAxisPlacement) {
+    case AxisPlacement.LEFT:
+      return 'end';
+    case AxisPlacement.BOTH:
+      throw new Error(
+        'BOTH is not a valid option for YAxis -- please specify RIGHT or LEFT'
+      );
+    case AxisPlacement.RIGHT:
+    case AxisPlacement.UNSPECIFIED:
+    default:
+      return 'start';
+  }
+};
 
-  renderZoomRect = () => (
-    <ZoomRect
-      width={this.props.width}
-      height={this.props.height}
-      zoomAxes={{ y: true }}
-      itemIds={[this.getItem().id]}
-    />
-  );
-
-  renderAxis() {
-    const {
-      defaultColor,
-      subDomainsByItemId,
-      height,
-      tickFormatter,
-      ticks,
-    } = this.props;
-
-    const item = this.getItem();
-    const color = item.color || defaultColor;
-    const scale = createYScale(Axes.y(subDomainsByItemId[item.id]), height);
-    const axis = d3.axisRight(scale);
-    const tickFontSize = 14;
-    const strokeWidth = 2;
-    const halfStrokeWidth = strokeWidth / 2;
-    const tickSizeOuter = axis.tickSizeOuter();
-    const tickSizeInner = axis.tickSizeInner();
-    const tickPadding = axis.tickPadding();
-    // same as for xAxis but consider height of the screen ~two times smaller
-    const nTicks = ticks || Math.floor(height / 50) || 1;
-    const values = scale.ticks(nTicks);
-    const range = scale.range().map(r => r + halfStrokeWidth);
-    return (
-      <g
-        className="axis y-axis"
-        fill="none"
-        fontSize={tickFontSize}
-        textAnchor={this.getTextAnchor()}
-        strokeWidth={strokeWidth}
-      >
-        <path
-          stroke={color}
-          d={this.getPathString({ tickSizeOuter, range, strokeWidth })}
-        />
-        {values.map(v => {
-          const lineProps = {
-            stroke: color,
-            ...this.getLineProps({ tickSizeInner, strokeWidth }),
-          };
-
-          const textProps = {
-            fill: color,
-            dy: '0.32em',
-            ...this.getTextProps({ tickSizeInner, tickPadding, strokeWidth }),
-          };
-          return (
-            <g key={+v} opacity={1} transform={`translate(0, ${scale(v)})`}>
-              <line {...lineProps} />
-              <text className="tick-value" {...textProps}>
-                {tickFormatter(v, values)}
-              </text>
-            </g>
-          );
+const YAxis = ({
+  collection,
+  defaultColor,
+  height,
+  offsetx,
+  onMouseEnter,
+  onMouseLeave,
+  series,
+  subDomainsByItemId,
+  tickFormatter,
+  ticks,
+  width,
+  yAxisPlacement,
+  zoomable,
+}) => {
+  const item = getItem(series, collection);
+  const color = item.color || defaultColor;
+  const scale = createYScale(Axes.y(subDomainsByItemId[item.id]), height);
+  const axis = d3.axisRight(scale);
+  const tickFontSize = 14;
+  const strokeWidth = 2;
+  const halfStrokeWidth = strokeWidth / 2;
+  const tickSizeOuter = axis.tickSizeOuter();
+  const tickSizeInner = axis.tickSizeInner();
+  const tickPadding = axis.tickPadding();
+  // same as for xAxis but consider height of the screen ~two times smaller
+  const nTicks = ticks || Math.floor(height / 50) || 1;
+  const values = scale.ticks(nTicks);
+  const range = scale.range().map(r => r + halfStrokeWidth);
+  const axisElement = (
+    <g
+      className="axis y-axis"
+      fill="none"
+      fontSize={tickFontSize}
+      textAnchor={getTextAnchor({ yAxisPlacement })}
+      strokeWidth={strokeWidth}
+    >
+      <path
+        stroke={color}
+        d={getPathString({
+          range,
+          strokeWidth,
+          tickSizeOuter,
+          width,
+          yAxisPlacement,
         })}
-      </g>
-    );
-  }
+      />
+      {values.map(v => {
+        const lineProps = {
+          stroke: color,
+          ...getLineProps({
+            strokeWidth,
+            tickSizeInner,
+            width,
+            yAxisPlacement,
+          }),
+        };
 
-  render() {
-    const { offsetx, zoomable, onMouseEnter, onMouseLeave } = this.props;
-    const cursor = zoomable ? 'move' : 'inherit';
-    return (
-      <g
-        className="axis-y"
-        transform={`translate(${offsetx}, 0)`}
-        cursor={cursor}
-        onMouseEnter={onMouseEnter}
-        onMouseLeave={onMouseLeave}
-      >
-        {this.renderAxis()}
-        {zoomable && this.renderZoomRect()}
-      </g>
-    );
-  }
-}
+        const textProps = {
+          fill: color,
+          dy: '0.32em',
+          ...getTextProps({
+            strokeWidth,
+            tickPadding,
+            tickSizeInner,
+            width,
+            yAxisPlacement,
+          }),
+        };
+        return (
+          <g key={+v} opacity={1} transform={`translate(0, ${scale(v)})`}>
+            <line {...lineProps} />
+            <text className="tick-value" {...textProps}>
+              {tickFormatter(v, values)}
+            </text>
+          </g>
+        );
+      })}
+    </g>
+  );
+  const cursor = zoomable ? 'move' : 'inherit';
+  return (
+    <g
+      className="axis-y"
+      transform={`translate(${offsetx}, 0)`}
+      cursor={cursor}
+      onMouseEnter={onMouseEnter}
+      onMouseLeave={onMouseLeave}
+    >
+      {axisElement}
+      {zoomable && (
+        <ZoomRect
+          width={width}
+          height={height}
+          zoomAxes={{ y: true }}
+          itemIds={[getItem(series, collection).id]}
+        />
+      )}
+    </g>
+  );
+};
 
 YAxis.propTypes = propTypes;
 YAxis.defaultProps = defaultProps;
 
 export default props => (
   <ScalerContext.Consumer>
-    {({ subDomainsByItemId, updateDomains }) => (
-      <YAxis
-        {...props}
-        subDomainsByItemId={subDomainsByItemId}
-        updateDomains={updateDomains}
-      />
+    {({ subDomainsByItemId }) => (
+      <YAxis {...props} subDomainsByItemId={subDomainsByItemId} />
     )}
   </ScalerContext.Consumer>
 );
