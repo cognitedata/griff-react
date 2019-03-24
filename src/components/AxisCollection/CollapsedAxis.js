b865796d76b711c757e69deea96cf56c4da06f0b
Evan Charlton <evan.charlton@cognite.com>
2019-03-24 12:15:35 +0100

    Convert CollapsedAxis to a functional component
    This component has no state; there's no reason it should be an ES6
    class. Instead, convert it to be a functional component.

--- src/components/AxisCollection/CollapsedAxis.js
+++ src/components/AxisCollection/CollapsedAxis.js
@@ -1,65 +1,78 @@
-import React, { Component } from 'react';
+import React from 'react';
 import * as d3 from 'd3';
 import PropTypes from 'prop-types';
 import { createYScale } from '../../utils/scale-helpers';
 import GriffPropTypes from '../../utils/proptypes';
 import AxisPlacement from '../AxisPlacement';
 
-export default class CollapsedAxis extends Component {
-  static propTypes = {
-    height: PropTypes.number.isRequired,
-    width: PropTypes.number.isRequired,
-    offsetx: PropTypes.number,
-    color: PropTypes.string,
-    onMouseEnter: PropTypes.func,
-    onMouseLeave: PropTypes.func,
-    yAxisPlacement: GriffPropTypes.axisPlacement,
-  };
+const propTypes = {
+  height: PropTypes.number.isRequired,
+  width: PropTypes.number.isRequired,
+  offsetx: PropTypes.number,
+  color: PropTypes.string,
+  onMouseEnter: PropTypes.func,
+  onMouseLeave: PropTypes.func,
+  yAxisPlacement: GriffPropTypes.axisPlacement,
+};
 
-  static defaultProps = {
-    color: '#666',
-    offsetx: 0,
-    onMouseEnter: null,
-    onMouseLeave: null,
-    yAxisPlacement: AxisPlacement.RIGHT,
-  };
+const defaultProps = {
+  color: '#666',
+  offsetx: 0,
+  onMouseEnter: null,
+  onMouseLeave: null,
+  yAxisPlacement: AxisPlacement.RIGHT,
+};
 
-  getPath = ({ offsetx, strokeWidth, tickSizeOuter, range }) => {
-    const { yAxisPlacement, width } = this.props;
-    switch (yAxisPlacement) {
-      case AxisPlacement.LEFT:
-        return [
-          // Move to this (x,y); start drawing
-          `M ${width - offsetx} ${strokeWidth}`,
-          // Draw a horizontal line to the left
-          `h ${tickSizeOuter - strokeWidth}`,
-          // Draw a vertical line from top to bottom
-          `v ${range[0] - strokeWidth * 2}`,
-          // Finish with another horizontal line
-          `h -${tickSizeOuter - strokeWidth / 2}`,
-        ].join(' ');
-      case AxisPlacement.BOTH:
-        throw new Error(
-          'BOTH is not a valid option for CollapsedAxis -- please specify RIGHT or LEFT'
-        );
-      case AxisPlacement.RIGHT:
-      case AxisPlacement.UNSPECIFIED:
-      default:
-        return [
-          // Move to this (x,y); start drawing
-          `M ${offsetx} ${strokeWidth}`,
-          // Draw a horizontal line to the left
-          `h -${tickSizeOuter - strokeWidth}`,
-          // Draw a vertical line from top to bottom
-          `v ${range[0] - strokeWidth * 2}`,
-          // Finish with another horizontal line
-          `h ${tickSizeOuter - strokeWidth / 2}`,
-        ].join(' ');
-    }
-  };
+const getPath = ({
+  offsetx,
+  strokeWidth,
+  tickSizeOuter,
+  range,
+  yAxisPlacement,
+  width,
+}) => {
+  switch (yAxisPlacement) {
+    case AxisPlacement.LEFT:
+      return [
+        // Move to this (x,y); start drawing
+        `M ${width - offsetx} ${strokeWidth}`,
+        // Draw a horizontal line to the left
+        `h ${tickSizeOuter - strokeWidth}`,
+        // Draw a vertical line from top to bottom
+        `v ${range[0] - strokeWidth * 2}`,
+        // Finish with another horizontal line
+        `h -${tickSizeOuter - strokeWidth / 2}`,
+      ].join(' ');
+    case AxisPlacement.BOTH:
+      throw new Error(
+        'BOTH is not a valid option for CollapsedAxis -- please specify RIGHT or LEFT'
+      );
+    case AxisPlacement.RIGHT:
+    case AxisPlacement.UNSPECIFIED:
+    default:
+      return [
+        // Move to this (x,y); start drawing
+        `M ${offsetx} ${strokeWidth}`,
+        // Draw a horizontal line to the left
+        `h -${tickSizeOuter - strokeWidth}`,
+        // Draw a vertical line from top to bottom
+        `v ${range[0] - strokeWidth * 2}`,
+        // Finish with another horizontal line
+        `h ${tickSizeOuter - strokeWidth / 2}`,
+      ].join(' ');
+  }
+};
 
-  renderAxis() {
-    const { color, height, onMouseEnter, onMouseLeave } = this.props;
+const CollapsedAxis = ({
+  height,
+  width,
+  offsetx: initialOffsetX,
+  color,
+  onMouseEnter,
+  onMouseLeave,
+  yAxisPlacement,
+}) => {
+  const renderAxis = () => {
     const scale = createYScale([0, 100], height);
     const axis = d3.axisRight(scale);
     const tickFontSize = 14;
@@ -71,7 +84,14 @@ export default class CollapsedAxis extends Component {
     for (let i = 1; i < 4; i += 1) {
       offsetx += tickSizeOuter;
       paths[i] = {
-        path: this.getPath({ offsetx, strokeWidth, tickSizeOuter, range }),
+        path: getPath({
+          offsetx,
+          strokeWidth,
+          tickSizeOuter,
+          range,
+          yAxisPlacement,
+          width,
+        }),
         color,
         opacity: 1 - (i - 1) / 4,
       };
@@ -98,17 +118,19 @@ export default class CollapsedAxis extends Component {
         ))}
       </g>
     );
-  }
+  };
 
-  render() {
-    const { offsetx } = this.props;
-    return (
-      <g
-        className="axis-y collapsed-axis-y"
-        transform={`translate(${offsetx}, 0)`}
-      >
-        {this.renderAxis()}
-      </g>
-    );
-  }
-}
+  return (
+    <g
+      className="axis-y collapsed-axis-y"
+      transform={`translate(${initialOffsetX}, 0)`}
+    >
+      {renderAxis()}
+    </g>
+  );
+};
+
+CollapsedAxis.propTypes = propTypes;
+CollapsedAxis.defaultProps = defaultProps;
+
+export default CollapsedAxis;
