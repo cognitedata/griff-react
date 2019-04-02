0815a630e056f9f04b9977b091b7491cbb64e02d
Evan Charlton <evan.charlton@cognite.com>
2019-04-02 07:32:09 +0200

    Ported Points and PointCollection to TypeScript
    These two come as a pair, so it makes sense to port them at the same
    time.
    
    Also move tsc to use `moduleResolution: node` to handle the default
    imports/exports.

--- src/components/PointCollection/index.tsx
+++ src/components/PointCollection/index.tsx
@@ -1,10 +1,20 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import * as React from 'react';
+import * as PropTypes from 'prop-types';
 import ScalerContext from '../../context/Scaler';
 import { createYScale, createXScale } from '../../utils/scale-helpers';
-import Points from '../Points';
 import GriffPropTypes, { seriesPropType } from '../../utils/proptypes';
 import Axes from '../../utils/Axes';
+import { SizeProps } from '../../internal';
+import { Series, Datapoint } from '../../external';
+import { DomainsByItemId } from '../Scaler';
+import Points from '../Points';
+
+export interface Props {}
+
+interface InternalProps {
+  series: Series[];
+  subDomainsByItemId: DomainsByItemId;
+}
 
 const propTypes = {
   width: PropTypes.number.isRequired,
@@ -14,7 +24,9 @@ const propTypes = {
 };
 const defaultProps = {};
 
-const PointCollection = ({ width, height, series, subDomainsByItemId }) => {
+const PointCollection: React.FunctionComponent<
+  Props & SizeProps & InternalProps
+> = ({ width, height, series, subDomainsByItemId }) => {
   const points = series
     .filter(s => !s.hidden && s.drawPoints !== false)
     .map(s => {
@@ -32,8 +44,8 @@ const PointCollection = ({ width, height, series, subDomainsByItemId }) => {
       // infinity so that they go to the ends of the graph, but points are special
       // since they can overlap in the [x,y] plane, but not be in the current time
       // subdomain.
-      const pointFilter = d => {
-        const timestamp = s.timeAccessor(d);
+      const pointFilter = (d: Datapoint, i: number, arr: Datapoint[]) => {
+        const timestamp = s.timeAccessor(d, i, arr);
         const subDomain = Axes.time(subDomainsByItemId[s.id]);
         return subDomain[0] <= timestamp && timestamp <= subDomain[1];
       };
@@ -61,9 +73,9 @@ const PointCollection = ({ width, height, series, subDomainsByItemId }) => {
 PointCollection.propTypes = propTypes;
 PointCollection.defaultProps = defaultProps;
 
-export default props => (
+export default (props: Props & SizeProps) => (
   <ScalerContext.Consumer>
-    {({ subDomainsByItemId, series }) => (
+    {({ subDomainsByItemId, series }: InternalProps) => (
       <PointCollection
         {...props}
         series={series}
--- src/external.d.ts
+++ src/external.d.ts
@@ -5,6 +5,43 @@ export type Domain = [number, number];
 
 export type ItemId = string | number;
 
-export interface Series extends Item {}
+export interface Series extends Item {
+  collectionId?: ItemId;
+}
 
 export interface Collection extends Item {}
+
+export interface Datapoint {
+  timestamp?: number;
+  x?: number;
+  y?: number;
+}
+
+export type AccessorFunction = (
+  d: Datapoint,
+  i: number,
+  arr: Datapoint[]
+) => number;
+
+export interface PointRendererMetadata {
+  x: number;
+  x0?: number;
+  x1?: number;
+  y: number;
+  y0?: number;
+  y1?: number;
+  color?: string;
+  opacity?: number;
+  opacityAccessor?: AccessorFunction;
+  pointWidth?: number;
+  pointWidthAccessor?: AccessorFunction;
+  strokeWidth?: number;
+}
+
+export type PointRenderer = (
+  d: Datapoint,
+  i: number,
+  data: Datapoint[],
+  metadata: PointRendererMetadata,
+  uiElements: React.ReactElement[]
+) => React.ReactElement[] | null;
