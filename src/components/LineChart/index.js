389f603d3d4701420efe3790745904bafaad0c40
Torbjørn Follan <tobbis.follan@gmail.com>
2019-05-07 14:12:06 +0200

    Ensure line chart height and width are positive (#310)
    Use max of 0 and calculated width / height for LineChart size.
    LineChart could get negative width and height, giving error messages in the console.

--- src/components/LineChart/index.js
+++ src/components/LineChart/index.js
@@ -267,12 +267,14 @@ const LineChart = props => {
   const width = propWidth || sizeWidth;
   const height = propHeight || sizeHeight;
   const contextChartSpace = getContextChartHeight(props);
+  const chartWidth =
+    width -
+    getYAxisCollectionWidth(AxisPlacement.LEFT, props) -
+    getYAxisCollectionWidth(AxisPlacement.RIGHT, props);
+  const chartHeight = height - getXAxisHeight(xAxisHeight) - contextChartSpace;
   const chartSize = {
-    width:
-      width -
-      getYAxisCollectionWidth(AxisPlacement.LEFT, props) -
-      getYAxisCollectionWidth(AxisPlacement.RIGHT, props),
-    height: height - getXAxisHeight(xAxisHeight) - contextChartSpace,
+    width: Math.max(0, chartWidth),
+    height: Math.max(0, chartHeight),
   };
 
   return (
