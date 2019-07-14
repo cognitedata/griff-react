183dd095a3a0c6aa031bc9368dcbbc8042d7de91
Evan Charlton <evan.charlton@cognite.com>
2019-07-14 12:11:26 +0200

    Fix the x-axis placement stories

--- stories/XAxisPlacements.stories.js
+++ stories/XAxisPlacements.stories.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import {
-  DataProvider,
+  Griff,
   LineChart,
   AxisPlacement,
   Series,
@@ -12,92 +12,60 @@ import { staticLoader } from './loaders';
 const staticXDomain = [Date.now() - 1000 * 60 * 60 * 24 * 30, Date.now()];
 const CHART_HEIGHT = 500;
 
-storiesOf('X-Axis Placement', module)
+storiesOf('Axes/placement/X', module)
   .add('Unspecified', () => [
-    <DataProvider
-      key="series"
-      defaultLoader={staticLoader}
-      timeDomain={staticXDomain}
-    >
+    <Griff key="series" loader={staticLoader} timeDomain={staticXDomain}>
       <Series id="1" color="steelblue" />
       <Series id="2" color="maroon" />
       <LineChart height={CHART_HEIGHT} />
-    </DataProvider>,
-    <DataProvider
-      key="collections"
-      defaultLoader={staticLoader}
-      timeDomain={staticXDomain}
-    >
+    </Griff>,
+    <Griff key="collections" loader={staticLoader} timeDomain={staticXDomain}>
       <Collection id="all" color="red">
         <Series id="1" color="steelblue" />
         <Series id="2" color="maroon" />
       </Collection>
       <LineChart height={CHART_HEIGHT} />
-    </DataProvider>,
+    </Griff>,
   ])
   .add('Top', () => [
-    <DataProvider
-      key="series"
-      defaultLoader={staticLoader}
-      timeDomain={staticXDomain}
-    >
+    <Griff key="series" loader={staticLoader} timeDomain={staticXDomain}>
       <Series id="1" color="steelblue" />
       <Series id="2" color="maroon" />
       <LineChart height={CHART_HEIGHT} xAxisPlacement={AxisPlacement.TOP} />
-    </DataProvider>,
-    <DataProvider
-      key="collections"
-      defaultLoader={staticLoader}
-      timeDomain={staticXDomain}
-    >
+    </Griff>,
+    <Griff key="collections" loader={staticLoader} timeDomain={staticXDomain}>
       <Collection id="all" color="red">
         <Series id="1" color="steelblue" />
         <Series id="2" color="maroon" />
       </Collection>
       <LineChart height={CHART_HEIGHT} xAxisPlacement={AxisPlacement.TOP} />
-    </DataProvider>,
+    </Griff>,
   ])
   .add('Bottom', () => [
-    <DataProvider
-      key="series"
-      defaultLoader={staticLoader}
-      timeDomain={staticXDomain}
-    >
+    <Griff key="series" loader={staticLoader} timeDomain={staticXDomain}>
       <Series id="1" color="steelblue" />
       <Series id="2" color="maroon" />
       <LineChart height={CHART_HEIGHT} xAxisPlacement={AxisPlacement.BOTTOM} />
-    </DataProvider>,
-    <DataProvider
-      key="collections"
-      defaultLoader={staticLoader}
-      timeDomain={staticXDomain}
-    >
+    </Griff>,
+    <Griff key="collections" loader={staticLoader} timeDomain={staticXDomain}>
       <Collection id="all" color="red">
         <Series id="1" color="steelblue" />
         <Series id="2" color="maroon" />
       </Collection>
       <LineChart height={CHART_HEIGHT} xAxisPlacement={AxisPlacement.BOTTOM} />
-    </DataProvider>,
+    </Griff>,
   ])
   .add('Both', () => [
-    <DataProvider
-      key="series"
-      defaultLoader={staticLoader}
-      timeDomain={staticXDomain}
-    >
+    <Griff key="series" loader={staticLoader} timeDomain={staticXDomain}>
       <Series id="1" color="steelblue" />
       <Series id="2" color="maroon" />
       <LineChart height={CHART_HEIGHT} xAxisPlacement={AxisPlacement.BOTH} />
-    </DataProvider>,
-    <DataProvider
-      key="collections"
-      defaultLoader={staticLoader}
-      timeDomain={staticXDomain}
-    >
+    </Griff>,
+    <Griff key="collections" loader={staticLoader} timeDomain={staticXDomain}>
       <Collection id="all" color="red">
         <Series id="1" color="steelblue" />
         <Series id="2" color="maroon" />
       </Collection>
       <LineChart height={CHART_HEIGHT} xAxisPlacement={AxisPlacement.BOTH} />
-    </DataProvider>,
+    </Griff>,
   ]);
