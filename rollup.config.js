9b16ec7b2c7dcd8a432ac6778dff38f555bde184
Evan Charlton <evan.charlton@cognite.com>
2019-11-11 18:39:32 +0000

    Exclude helper files (stories, tests) from the build (#333)
    * Exclude helper files (stories, tests) from the build
    
    Set it up to allow stories to be colocatd with the components but also
    excluded from the build.

--- rollup.config.js
+++ rollup.config.js
@@ -23,6 +23,7 @@ export default {
     typescript({
       typescript: typescriptPkg,
       include: ['*.js', '**/*.js', '*.ts', '**/*.ts', '*.tsx', '**/*.tsx'],
+      exclude: ['*.stories.*', '*.spec.*'],
     }),
     json(),
   ],
