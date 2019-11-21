58cbfa3fe93e3e79d61ca225f300443e9ee764ad
jtaaa <joshua.allum@cognite.com>
2019-11-21 15:57:37 +0100

    Add x and y offset options to createTrigValueFunc

--- src/storybook/utils/loaders/loaders.ts
+++ src/storybook/utils/loaders/loaders.ts
@@ -2,20 +2,31 @@ import { ValueFunc, LoaderFunc } from './types';
 
 const POINT_COUNT_DEFAULT = 250;
 
+type CreateTrigValueFuncOptions = {
+  oscillations?: number;
+  yOffset?: number;
+  xOffset?: number;
+};
+
 export const createTrigValueFunc = (
   trigFn: (n: number) => number,
-  oscillations: number = 1
-): ValueFunc => (i, n) => trigFn(2 * Math.PI * (i / n) * oscillations);
+  {
+    oscillations = 1,
+    yOffset = 0,
+    xOffset = 0,
+  }: CreateTrigValueFuncOptions = {}
+): ValueFunc => (i, n) =>
+  trigFn(xOffset + 2 * Math.PI * (i / n) * oscillations) + yOffset;
 
-type CreateStaticLoaderProps = {
+type CreateStaticLoaderOptions = {
   pointCount?: number;
   valueFunc?: ValueFunc;
 };
 
 export const createStaticLoader = ({
   pointCount = POINT_COUNT_DEFAULT,
-  valueFunc = createTrigValueFunc(Math.sin, 1),
-}: CreateStaticLoaderProps = {}): LoaderFunc => ({ timeDomain }) => {
+  valueFunc = createTrigValueFunc(Math.sin),
+}: CreateStaticLoaderOptions = {}): LoaderFunc => ({ timeDomain }) => {
   const diff = timeDomain[1] - timeDomain[0];
   return {
     data: Array(pointCount)
