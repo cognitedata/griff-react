6cc5c54ab1943661cea3cad8b884494d1224d8b7
Joshua Allum <joshuatallum@gmail.com>
2019-11-22 10:30:48 +0100

    Stories and snapshots (#340)
    * Add all stories in stories folder to Demo group
    * Use @cognite/eslint-config and fix linting errors
    * Lint typescript and fix lint errors
    * Fix TypeScipt compilation errors
    * Add a new LineChart story
    * Setup jest properly
    
    Remove babel in favour of using ts-jest with project tsconfig
    * Add react-testing-library and first snapshot test
    * Re-add babel-eslint
    
    Since it's a peer dependency of cognite-eslint-config
    * Wrap story in fixed width container
    * LineChart y-axis stuff
    * Allow size prop override on sizeme components
    
    Update tests to use this to mock size.
    * Set test timezone to Olso time and update snapshot
    * Switch timezone to Oslo time
    * Bump react-sizeme and use types
    * fix: RulerTooltip story types
    * Simplify tsconfig include and include stories dir
    * Merge branch 'lint-and-fix-ts' into snapshot-test
    * Update snapshot due to using size prop default
    * Set baseUrl to clean up import paths (#339)
    
    * Set baseUrl for build, jest and storybook to src
    * Add babel-preset-react-app (used in story config)
    * Move storybook utils into source
    * Use imports from src baseUrl
    * Revert change to put storybook build in .out
    * Update loaders for more flexibility
    
    Update snapshot as a result of updated order of operations in
    calculations leading to small changes results due to error.
    * Add x and y offset options to createTrigValueFunc
    * Configure test setup properly
    * Add multiseries and collection stories and tests
    * Test in prepush script
    * Rename loaders.ts to createStaticLoader.ts
    * Remove unnecessary Fragments
    * Use consistent function abbreviation
    * Update yarn.lock
    * Update snaps due to slight rounding differences
    
    Maybe because of an updated package?
    * Merge branch 'master' into stories-and-snapshots
    * Update the yarn.lock again and remove loaders.ts

--- src/components/LineChart/LineChart.spec.tsx
+++ src/components/LineChart/LineChart.spec.tsx
@@ -1,19 +1,63 @@
 import React from 'react';
 import { render, wait } from '@testing-library/react';
-import { Base } from './LineChart.stories';
+import { Base, MultiSeries, MultiSeriesCollection } from './LineChart.stories';
 
 const DEFAULT_SIZE = { width: 1000, height: 500 };
 
 describe('LineChart', () => {
   it('should base render correctly', async () => {
-    const { asFragment, queryAllByTestId } = render(
-      <Base size={DEFAULT_SIZE} />
-    );
+    const {
+      asFragment,
+      getAllByTestId,
+      queryAllByTestId,
+      getByTestId,
+    } = render(<Base size={DEFAULT_SIZE} />);
 
     await wait(() => {
-      expect(queryAllByTestId('Line-1')).toHaveLength(2);
-      expect(queryAllByTestId('xAxis')).toHaveLength(2);
+      expect(queryAllByTestId('Line-Series-1')).toHaveLength(2);
     });
+    expect(getAllByTestId('xAxis')).toHaveLength(2);
+    expect(getByTestId('yAxis-Series-1')).toBeInTheDocument();
+    expect(asFragment()).toMatchSnapshot();
+  });
+
+  it('should render multiple series correctly', async () => {
+    const {
+      asFragment,
+      getAllByTestId,
+      queryAllByTestId,
+      getByTestId,
+    } = render(<MultiSeries size={DEFAULT_SIZE} />);
+
+    await wait(() => {
+      expect(queryAllByTestId('Line-Series-1')).toHaveLength(2);
+      expect(queryAllByTestId('Line-Series-2')).toHaveLength(2);
+      expect(queryAllByTestId('Line-Series-3')).toHaveLength(2);
+    });
+    expect(getAllByTestId('xAxis')).toHaveLength(2);
+    expect(getByTestId('yAxis-Series-1')).toBeInTheDocument();
+    expect(getByTestId('yAxis-Series-2')).toBeInTheDocument();
+    expect(getByTestId('yAxis-Series-3')).toBeInTheDocument();
+    expect(asFragment()).toMatchSnapshot();
+  });
+
+  it('should render multiple series in a single collection correctly', async () => {
+    const {
+      asFragment,
+      getAllByTestId,
+      queryAllByTestId,
+      getByTestId,
+      queryByTestId,
+    } = render(<MultiSeriesCollection size={DEFAULT_SIZE} />);
+
+    await wait(() => {
+      expect(queryAllByTestId('Line-Series-1')).toHaveLength(2);
+      expect(queryAllByTestId('Line-Series-2')).toHaveLength(2);
+      expect(queryAllByTestId('Line-Series-3')).toHaveLength(2);
+    });
+    expect(getAllByTestId('xAxis')).toHaveLength(2);
+    expect(getByTestId('yAxis-Collection-1')).toBeInTheDocument();
+    expect(queryByTestId('yAxis-Series-1')).not.toBeInTheDocument();
     expect(asFragment()).toMatchSnapshot();
   });
 });
--- src/components/LineChart/LineChart.stories.tsx
+++ src/components/LineChart/LineChart.stories.tsx
@@ -1,5 +1,11 @@
 import React from 'react';
-import { sineLoader, StoryContainer } from 'storybook/utils';
+import {
+  StoryContainer,
+  createStaticLoader,
+  createTrigValueFunc,
+} from 'storybook/utils';
+
+import Collection from 'components/Collection';
 import DataProvider from 'components/DataProvider';
 import Series from 'components/Series';
 import LineChart from '.';
@@ -18,9 +24,64 @@ export const Base = ({ size }: StoryProps) => {
   const timeDomain = [0, 10000000];
   return (
     <StoryContainer>
-      <DataProvider defaultLoader={sineLoader} timeDomain={timeDomain}>
-        <Series id="1" color="steelblue" />
+      <DataProvider
+        defaultLoader={createStaticLoader()}
+        timeDomain={timeDomain}
+      >
+        <Series id="Series-1" color="steelblue" />
         <LineChart height={DEFAULT_HEIGHT} size={size} />
+      </DataProvider>
+    </StoryContainer>
+  );
+};
+
+export const MultiSeries = ({ size }: StoryProps) => {
+  const timeDomain = [0, 100000000];
+  return (
+    <StoryContainer>
+      <DataProvider timeDomain={timeDomain}>
+        <Series id="Series-1" color="steelblue" loader={createStaticLoader()} />
+        <Series
+          id="Series-2"
+          color="maroon"
+          loader={createStaticLoader({
+            valueFunc: createTrigValueFunc(Math.cos),
+          })}
+        />
+        <Series
+          id="Series-3"
+          color="forestgreen"
+          loader={createStaticLoader({
+            valueFunc: createTrigValueFunc(Math.tan),
+          })}
+        />
+        <LineChart height={DEFAULT_HEIGHT} size={size} />
+      </DataProvider>
+    </StoryContainer>
+  );
+};
+
+export const MultiSeriesCollection = ({ size }: StoryProps) => {
+  const timeDomain = [0, 1000000000];
+  return (
+    <StoryContainer>
+      <DataProvider timeDomain={timeDomain}>
+        <Collection id="Collection-1" color="darkkhaki">
+          <Series id="Series-1" loader={createStaticLoader()} />
+          <Series
+            id="Series-2"
+            loader={createStaticLoader({
+              valueFunc: createTrigValueFunc(Math.cos),
+            })}
+          />
+          <Series
+            id="Series-3"
+            loader={createStaticLoader({
+              valueFunc: createTrigValueFunc(Math.tan),
+            })}
+          />
+          <LineChart height={DEFAULT_HEIGHT} size={size} />
+        </Collection>
       </DataProvider>
     </StoryContainer>
   );
