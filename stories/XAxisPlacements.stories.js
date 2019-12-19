d73c73708f4a6e3d267d38e8a74eda6489f02fa7
jtaaa <joshua.allum@cognite.com>
2019-12-19 16:38:35 +0100

    Fix remaining broken stories

--- stories/XAxisPlacements.stories.js
+++ stories/XAxisPlacements.stories.js
@@ -15,94 +15,110 @@ export default {
   title: 'Demo|X-Axis Placement',
 };
 
-export const unspecified = () => [
-  <DataProvider
-    key="series"
-    defaultLoader={staticLoader}
-    timeDomain={staticXDomain}
-  >
-    <Series id="1" color="steelblue" />
-    <Series id="2" color="maroon" />
-    <LineChart height={CHART_HEIGHT} />
-  </DataProvider>,
-  <DataProvider
-    key="collections"
-    defaultLoader={staticLoader}
-    timeDomain={staticXDomain}
-  >
-    <Collection id="all" color="red">
+export const unspecified = () => (
+  <>
+    <DataProvider
+      key="series"
+      defaultLoader={staticLoader}
+      timeDomain={staticXDomain}
+    >
       <Series id="1" color="steelblue" />
       <Series id="2" color="maroon" />
-    </Collection>
-    <LineChart height={CHART_HEIGHT} />
-  </DataProvider>,
-];
+      <LineChart height={CHART_HEIGHT} />
+    </DataProvider>
+    ,
+    <DataProvider
+      key="collections"
+      defaultLoader={staticLoader}
+      timeDomain={staticXDomain}
+    >
+      <Collection id="all" color="red">
+        <Series id="1" color="steelblue" />
+        <Series id="2" color="maroon" />
+      </Collection>
+      <LineChart height={CHART_HEIGHT} />
+    </DataProvider>
+    ,
+  </>
+);
 
-export const top = () => [
-  <DataProvider
-    key="series"
-    defaultLoader={staticLoader}
-    timeDomain={staticXDomain}
-  >
-    <Series id="1" color="steelblue" />
-    <Series id="2" color="maroon" />
-    <LineChart height={CHART_HEIGHT} xAxisPlacement={AxisPlacement.TOP} />
-  </DataProvider>,
-  <DataProvider
-    key="collections"
-    defaultLoader={staticLoader}
-    timeDomain={staticXDomain}
-  >
-    <Collection id="all" color="red">
+export const top = () => (
+  <>
+    <DataProvider
+      key="series"
+      defaultLoader={staticLoader}
+      timeDomain={staticXDomain}
+    >
       <Series id="1" color="steelblue" />
       <Series id="2" color="maroon" />
-    </Collection>
-    <LineChart height={CHART_HEIGHT} xAxisPlacement={AxisPlacement.TOP} />
-  </DataProvider>,
-];
+      <LineChart height={CHART_HEIGHT} xAxisPlacement={AxisPlacement.TOP} />
+    </DataProvider>
+    ,
+    <DataProvider
+      key="collections"
+      defaultLoader={staticLoader}
+      timeDomain={staticXDomain}
+    >
+      <Collection id="all" color="red">
+        <Series id="1" color="steelblue" />
+        <Series id="2" color="maroon" />
+      </Collection>
+      <LineChart height={CHART_HEIGHT} xAxisPlacement={AxisPlacement.TOP} />
+    </DataProvider>
+    ,
+  </>
+);
 
-export const bottom = () => [
-  <DataProvider
-    key="series"
-    defaultLoader={staticLoader}
-    timeDomain={staticXDomain}
-  >
-    <Series id="1" color="steelblue" />
-    <Series id="2" color="maroon" />
-    <LineChart height={CHART_HEIGHT} xAxisPlacement={AxisPlacement.BOTTOM} />
-  </DataProvider>,
-  <DataProvider
-    key="collections"
-    defaultLoader={staticLoader}
-    timeDomain={staticXDomain}
-  >
-    <Collection id="all" color="red">
+export const bottom = () => (
+  <>
+    <DataProvider
+      key="series"
+      defaultLoader={staticLoader}
+      timeDomain={staticXDomain}
+    >
       <Series id="1" color="steelblue" />
       <Series id="2" color="maroon" />
-    </Collection>
-    <LineChart height={CHART_HEIGHT} xAxisPlacement={AxisPlacement.BOTTOM} />
-  </DataProvider>,
-];
+      <LineChart height={CHART_HEIGHT} xAxisPlacement={AxisPlacement.BOTTOM} />
+    </DataProvider>
+    ,
+    <DataProvider
+      key="collections"
+      defaultLoader={staticLoader}
+      timeDomain={staticXDomain}
+    >
+      <Collection id="all" color="red">
+        <Series id="1" color="steelblue" />
+        <Series id="2" color="maroon" />
+      </Collection>
+      <LineChart height={CHART_HEIGHT} xAxisPlacement={AxisPlacement.BOTTOM} />
+    </DataProvider>
+    ,
+  </>
+);
 
-export const both = () => [
-  <DataProvider
-    key="series"
-    defaultLoader={staticLoader}
-    timeDomain={staticXDomain}
-  >
-    <Series id="1" color="steelblue" />
-    <Series id="2" color="maroon" />
-    <LineChart height={CHART_HEIGHT} xAxisPlacement={AxisPlacement.BOTH} />
-  </DataProvider>,
-  <DataProvider
-    key="collections"
-    defaultLoader={staticLoader}
-    timeDomain={staticXDomain}
-  >
-    <Collection id="all" color="red">
+export const both = () => (
+  <>
+    <DataProvider
+      key="series"
+      defaultLoader={staticLoader}
+      timeDomain={staticXDomain}
+    >
       <Series id="1" color="steelblue" />
       <Series id="2" color="maroon" />
-    </Collection>
-    <LineChart height={CHART_HEIGHT} xAxisPlacement={AxisPlacement.BOTH} />
-  </DataProvider>,
-];
+      <LineChart height={CHART_HEIGHT} xAxisPlacement={AxisPlacement.BOTH} />
+    </DataProvider>
+    ,
+    <DataProvider
+      key="collections"
+      defaultLoader={staticLoader}
+      timeDomain={staticXDomain}
+    >
+      <Collection id="all" color="red">
+        <Series id="1" color="steelblue" />
+        <Series id="2" color="maroon" />
+      </Collection>
+      <LineChart height={CHART_HEIGHT} xAxisPlacement={AxisPlacement.BOTH} />
+    </DataProvider>
+    ,
+  </>
+);
